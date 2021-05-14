57057355d17fd7f7bef31d1c98b3f9c96bd91487
prateek-code-22 <iamprateek22@gmail.com>
2021-05-14 20:51:19 +0530

    Added Animated Container inplace of button

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:welcomebuddy/Pages/WelcomePage.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool changeButton = false;
+  String name = " ";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,45 +22,93 @@ class HomePage extends StatelessWidget {
           "Login App",
           style: TextStyle(
             fontSize: 22,
+            color: Colors.white,
           ),
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Image.asset("assets/home.png", fit: BoxFit.cover),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "Welcome to login page!",
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                        hintText: "Enter the Username", labelText: "Username"),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: "Enter the Password", labelText: "Password"),
-                  ),
-                ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset("assets/home.png", fit: BoxFit.cover),
+              SizedBox(
+                height: 10.0,
               ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            ElevatedButton(
+              Text("Welcome $name!",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Enter the Username",
+                          labelText: "Username"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: "Enter the Password",
+                          labelText: "Password"),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    changeButton = true;
+                  });
+                  await Future.delayed(Duration(seconds: 1));
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => WelcomePage()),
+                  );
+                },
+
+                // Implemented Animated Container in place of Elevated Button
+                child: AnimatedContainer(
+                  width: changeButton ? 50 : 150.0,
+                  height: 50.0,
+                  alignment: Alignment.center,
+                  duration: Duration(seconds: 1),
+                  child: changeButton
+                      ? Icon(
+                          Icons.done_rounded,
+                          color: Colors.white,
+                          size: 30.0,
+                        )
+                      : Text(
+                          "Login",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                  decoration: BoxDecoration(
+                    //shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
+                    color: Color(0xff00BFA6),
+                    borderRadius: BorderRadius.circular(changeButton ? 50 : 8),
+                  ),
+                ),
+              ),
+
+              /*ElevatedButton(
               onPressed: () {
                 print("Button pressed!");
                 Navigator.push(
@@ -62,8 +118,9 @@ class HomePage extends StatelessWidget {
               },
               child: Text("Login"),
               style: TextButton.styleFrom(),
-            )
-          ],
+            )*/
+            ],
+          ),
         ),
       ),
     );
